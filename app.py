d8ccd839359b57eb9f701fc84a3bc5652b6a64ea
Mark Lewis <markglewis@gmail.com>
2020-10-16 16:49:11 -0400

    a fix to model

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 import json
+from random import randrange
+
 
 import pandas as pd
 import pickle
@@ -17,6 +19,8 @@ def response():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
     #prediction = predict_model(model, data=final, round = 0)
+
+
     data = {
         "type": "line",
         "data": {"labels":["1st","2nd", "3rd"],
@@ -24,15 +28,39 @@ def response():
         "label": "My First dataset",
         "backgroundColor": "rgb(255, 99, 132)",
         "borderColor": "rgb(255, 99, 132)",
-        "data": [20,10,15]
+        "data": [randrange(10),randrange(10),randrange(10)]
         }],
         "options": {}}
     }
     return jsonify(data)
 
+@app.route('/model', methods=['GET', 'POST'])
+def model():
+    load_model = pickle.load(open('static/bayes_count.sav','rb'))
+    count_vect = pickle.load(open('static/count_vect.sav','rb'))
+    int_features = [x for x in request.form.values()]
 
 
+    testheadlines = ['TSLA - Cybertruck expected to hit the market strong']
 
-    
-
-   
+    testheadlines = int_features
+    nr = len(testheadlines)
+    dftest = pd.DataFrame(columns=['headline','clean_headline'],index=np.arange(nr))
+    for ii in np.arange(nr):
+    	txt = testheadlines[ii]
+    	dftest.iloc[ii]['clean_headline'] = txt
+    print(dftest.head())
+    ser = dftest['clean_headline']
+    for val in ser.values[0:4]:
+    	print(val)
+    ser_count =  count_vect.transform(ser)
+    predictions = load_model.predict(ser_count)
+    ii=0
+    #for val in ser.values:
+    returnvalue = []
+    for ii in np.arange(nr):
+    	print('\n tweet, sentiment = ',testheadlines[ii],predictions[ii])
+    	returnvalue = str(testheadlines[ii]) + " " + str(predictions[ii])
+    	returnvalue = [str(testheadlines[ii]),str(predictions[ii])]
+    	ii += 1
+    return jsonify(returnvalue)
